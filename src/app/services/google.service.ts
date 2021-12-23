cd0d852914597abdf86b01896b43950fdf5396b2
Sjens <scjensen813@live.com>
2021-12-22 21:45:02 -0800

    add keys back to env

--- src/app/services/google.service.ts
+++ src/app/services/google.service.ts
@@ -2,13 +2,13 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from "@angular/common/http";
 import { DisplayDetails, VideoIds, VideoInfo } from 'src/app/interfaces/google';
 import { map, mergeMap } from "rxjs/operators";
-import { YouTubeInfo } from "../../assets/secrets";
+import { environment } from "../../environments/environment";
 
 @Injectable({
   providedIn: 'root'
 })
 export class GoogleService {
-  YouTubeConfig = YouTubeInfo
+  YouTubeConfig = environment.YouTubeInfo
   Result_Count = 20 //max 50
   videoIds: string[] = []
   videoObject: VideoIds | undefined
@@ -18,7 +18,7 @@ export class GoogleService {
   constructor(private httpClient: HttpClient) { }
 
   getList() {
-    return this.httpClient.get(`${this.YouTubeConfig.URL}search?key=${this.YouTubeConfig.API_KEY}&channelId=${this.YouTubeConfig.Channel_Id}&maxResults=${this.Result_Count}&order=date`)
+    return this.httpClient.get(`${this.YouTubeConfig.URL}search?key=${this.YouTubeConfig.API_KEY}&channelId=${this.YouTubeConfig.Channel_ID}&maxResults=${this.Result_Count}&order=date`)
       .pipe(
         map((res: any) => {
           this.videoObject = res;
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,8 @@
 export const environment = {
-  production: true
+  production: true,
+  YouTubeInfo: {
+    URL: 'https://www.googleapis.com/youtube/v3/',
+    API_KEY: 'AIzaSyDBrTSGE3i9Rgf1kFVc72SvmD7w-oYkYnE',
+    Channel_ID: 'UCE6LcUU-b6gVvCvh1SgUOtw'
+  }
 };
